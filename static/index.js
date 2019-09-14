b593ecc26198d335d60e87b30a7d8582d8fa4893
Daniel Delago <ddelago0596@gmail.com>
2019-09-14 01:02:23 -0500

    js modules

---
+++ static/index.js
@@ -0,0 +1,17 @@
+// import { sidebar } from './sidebar.js';
+// import { view } from './view.js';
+// import { header } from './header.js';
+
+var container = 
+`
+<div class="container-fluid">
+    <div class="row">
+    helloo
+    </div>
+</div>
+`
+var app = document.getElementById("app");
+app.innerHTML = container;
+
+var appRow = app.querySelector(".row");
+
---
+++ static/modules/table.js
@@ -0,0 +1,25 @@
+// {/* <tr>
+//     <td>1,001</td>
+//     <td>Lorem</td>
+//     <td>ipsum</td>
+//     <td>dolor</td>
+//     <td>sit</td>
+// </tr>
+// <tr>
+//     <td>1,002</td>
+//     <td>amet</td>
+//     <td>consectetur</td>
+//     <td>adipiscing</td>
+//     <td>elit</td>
+// </tr> */}
+
+
+// function createRow(ctx, length, x, y, color) {
+    
+//     return {
+//       length: length,
+//       x: x,
+//       y: y,
+//       color: color
+//     };
+//   }
