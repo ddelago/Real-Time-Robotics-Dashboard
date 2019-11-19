e630871532cd99f0ea4ee8874c707d0e9f617f93
Daniel Delago <ddelago0596@gmail.com>
2019-11-18 23:14:58 -0600

    comments

--- static/common/variables.js
+++ static/common/variables.js
@@ -18,6 +18,7 @@ function load(url)
     return(req.responseText); 
 }
 
+// To avoid injected scripts error, pages have to be loaded with their hmtl and script seperate
 function loadPage(page)
 {
     var page_html = {
