d0705b5b89307a2ba3ee8c4d514f0c34a65f1545
Daniel Delago <daniel.delago@ayokasystems.com>
2020-02-19 22:28:25 -0600

    Icons

--- static/modules/sidebar/sidebar.mjs
+++ static/modules/sidebar/sidebar.mjs
@@ -1,4 +1,10 @@
 import { activePage, changeActivePage, pages, socket } from '../../common/variables.mjs';
+import {home, map, camera, arm} from '../../assets/icons.mjs';
+
+$(".home").html(home);
+$(".camera").html(camera);
+$(".arm").html(arm);
+$(".map").html(map);
 
 // For each nav-link element, add an onClick function to change active state
 document.querySelectorAll(".nav-link").forEach(function(navLink) {
@@ -14,7 +20,7 @@ document.querySelectorAll(".nav-link").forEach(function(navLink) {
         navLink.classList.add('active');
         
         // Change active page 
-        let newPage = e.target.innerHTML;
+        let newPage = e.target.id;
         document.querySelector(".active-page-header").innerHTML = `${newPage}`; 
         if (newPage != activePage) {
             $('#mainContent').html(pages[newPage]);
